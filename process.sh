a050954ecc9f0bf1c36be67bfdce53f510ff42a3
gogo40 <pericles.raskolnikoff@gmail.com>
2014-05-12 00:52:17 +0000

    Aprimorado servico ssh

--- process.sh
+++ process.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo $1
+
+echo "PATH=$1"
+
+cd $1
+
 TIMESTAMP=$(date -d "today" +"%Y%m%d%H%M")
 
-php mk_index.php
+php $1/mk_index.php
 
-mv fundamentus.json fundamentus_$TIMESTAMP.json
-mv fundamentus_mk.json fundamentus_mk_$TIMESTAMP.json
-mv fundamentus_raw.json fundamentus_raw_$TIMESTAMP.json
+mv $1/fundamentus.json $1/fundamentus_$TIMESTAMP.json
+mv $1/fundamentus_mk.json $1/fundamentus_mk_$TIMESTAMP.json
+mv $1/fundamentus_raw.json $1/fundamentus_raw_$TIMESTAMP.json
