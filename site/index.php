a379212d27585b984f67ab0af11638e576f943e6
root <root@debian.debian>
2014-06-19 23:20:40 -0300

    mk index site added

---
+++ site/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+(c) 2013, 2014, GoGo40 - Pericles Lopes Machado
+
+Script PHP para automaticamente baixar o database de 12 em 12 horas e 
+enviar email para a lista de usuários cadastrada
+*/
+
+require_once (__DIR__.'/mk_config.php');
+
