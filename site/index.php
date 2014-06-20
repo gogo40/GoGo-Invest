74480293ea6e9721679dc8faae8df757fe205ee2
root <root@debian.debian>
2014-06-19 23:35:41 -0300

    mkindex page initial code

--- site/index.php
+++ site/index.php
@@ -6,5 +6,12 @@ Script PHP para automaticamente baixar o database de 12 em 12 horas e
 enviar email para a lista de usuários cadastrada
 */
 
-require_once (__DIR__.'/mk_config.php');
+echo "Ola<p>";
+echo __DIR__."<p>";
 
+require_once (__DIR__.'/../mk_config.php');
+
+
+$files = glob(__DIR__."/../fundamentus_mk_*.json");
+
+print_r($files);
